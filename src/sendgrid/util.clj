73b1f6fa77b5b57bd41424c84aea4e5cd935d373
Anthon Taylor <anthonjtaylor@gmail.com>
2017-07-07 10:47:07 -0700

    basic-get

--- src/sendgrid/util.clj
+++ src/sendgrid/util.clj
@@ -1,2 +1,6 @@
 (ns sendgrid.util
   (:require [clj-http.client :as client]))
+
+;;This works
+(def API_TOKEN "Bearer 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")
+(client/get "https://api.sendgrid.com/v3/templates" {:headers {:authorization API_TOKEN :content-type :json}})
