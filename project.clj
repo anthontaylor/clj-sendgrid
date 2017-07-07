c568ce6f1f45ea4d0a4696cceaa03db61332afd8
Anthon Taylor <anthonjtaylor@gmail.com>
2017-07-06 17:10:32 -0700

    added deps

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.6.1"]
+                 [environ "1.1.0"]
+                 [cheshire "5.7.1"]
+                 [clj-http "3.6.1"]]
+  :plugins [[lein-environ "1.1.0"]])
