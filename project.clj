7abadb0d92cb44fe9a6d42cb264ed21b37881e3d
Anthon Taylor <anthonjtaylor@gmail.com>
2017-07-11 16:59:39 -0700

    update project name

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject sendgrid "0.1.0"
+(defproject clj-sendgrid "0.1.0"
   :description "A Clojure Library for the Sendgrid API v3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "http://slaejae.com"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.6.1"]
                  [environ "1.1.0"]
@@ -10,4 +11,14 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.codec "0.1.0"]]
   :plugins [[lein-environ "1.1.0"]
-            [lein-cloverage "1.0.7-SNAPSHOT"]])
+            [lein-cloverage "1.0.7-SNAPSHOT"]]
+  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                                     :creds :gpg}
+                         "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
+                                      :creds :gpg}]]
+  :scm {:url "git@github.com:anthontaylor/clj-sendgrid.git"}
+  :pom-addition [:developers [:developer
+                              [:name "AJ Taylor"]
+                              [:url "http://slaejae.com"]
+                              [:email "info@slaejae.com"]
+                              [:timezone "-5"]]])
