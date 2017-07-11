268c4cc551d5f795520a4e27e486c725e10eb9e8
Anthon Taylor <anthonjtaylor@gmail.com>
2017-07-11 15:34:32 -0700

    restructured

--- src/sendgrid/email.clj
+++ src/sendgrid/email.clj
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Testing requests (#3)
 (ns sendgrid.email
   (:require [clj-http.client :as client]
             [clojure.data.json :refer [write-str]]
@@ -9,7 +5,6 @@
             [sendgrid.util :refer [url]]))
 
 (defn send-email
-<<<<<<< HEAD
   [{:keys [from to subject message api-token filename content]}]
   (let [json-body {:personalizations [{:to [{"email" to}]}]
                    :from    {"email" from}
@@ -20,18 +15,3 @@
                  :headers {:authorization api-token}
                  :body (write-str (merge json-body attachments))}]
     (client/post (str url "mail/send") request)))
-=======
-(ns sendgrid.email)
->>>>>>> Basic request
-=======
-  [{:keys [from to subject message api-token]}]
-  (client/post
-   (str url "mail/send")
-   {:content-type :json
-    :headers {:authorization api-token}
-    :body (write-str {:personalizations [{:to [{"email" to}]}]
-                      :from    {"email" from}
-                      :subject  subject
-                      :content [{:type "text/html"
-                                 :value  message}]})}))
->>>>>>> Testing requests (#3)
