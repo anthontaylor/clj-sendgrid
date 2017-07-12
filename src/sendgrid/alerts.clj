159baebdfc936e88bd24f2561d69be77fe24eca5
Anthon Taylor <anthonjtaylor@gmail.com>
2017-07-11 19:17:34 -0700

    game plan

--- src/sendgrid/alerts.clj
+++ src/sendgrid/alerts.clj
@@ -4,8 +4,17 @@
             [sendgrid.util :refer [url]]
             [environ.core :refer [env]]))
 
+;;GET https://api.sendgrid.com/v3/alerts HTTP/1.1
 (defn alerts
   [{api-token :api-token}]
   (client/get (str url "alerts")
               {:headers {:authorization api-token
                          :content-type :json}}))
+
+;;POST https://api.sendgrid.com/v3/alerts HTTP/1.1
+
+;;GET https://api.sendgrid.com/v3/alerts/{alert_id} HTTP/1.1
+
+;;DELETE https://api.sendgrid.com/v3/alerts/{alert_id} HTTP/1.1
+
+;;PATCH https://api.sendgrid.com/v3/alerts/{alert_id} HTTP/1.1
--- src/sendgrid/ip_access_management.clj
+++ src/sendgrid/ip_access_management.clj
@@ -1,1 +1,13 @@
 (ns sendgrid.ip-access-management)
+
+;;GET https://api.sendgrid.com/v3/access_settings/activity?limit={limit} HTTP/1.1
+
+;;GET https://api.sendgrid.com/v3/access_settings/whitelist HTTP/1.1
+
+;;POST https://api.sendgrid.com/v3/access_settings/whitelist HTTP/1.1
+
+;;DELETE https://api.sendgrid.com/v3/access_settings/whitelist HTTP/1.1
+
+;;GET https://api.sendgrid.com/v3/access_settings/whitelist/{rule_id} HTTP/1.1
+
+;;DELETE https://api.sendgrid.com/v3/access_settings/whitelist/{rule_id} HTTP/1.1
