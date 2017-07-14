d126612a50f2b82214d6a92a7ea4e3993c5c7d98
Anthon Taylor <anthonjtaylor@gmail.com>
2017-07-14 09:17:32 -0700

    blah

--- src/sendgrid/core.clj
+++ src/sendgrid/core.clj
@@ -1,7 +1,6 @@
 (ns sendgrid.core
   (:require [clj-http.client :as client]
             [clojure.data.json :refer [write-str]]
-            [sendgrid.util :refer [url]]
             [environ.core :refer [env]]))
 
 (def url "https://api.sendgrid.com/v3/")
