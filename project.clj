92fffc27a9549adbdef63f41614fe7f13061b46d
AJ Taylor <anthonjtaylor@gmail.com>
2017-07-14 09:42:45 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-sendgrid "0.1.1"
+(defproject clj-sendgrid "0.1.2"
   :description "A Clojure Library for the Sendgrid API v3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
