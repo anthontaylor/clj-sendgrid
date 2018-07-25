296b4f794595f3303bd2db8ada06b52a805b51a8
Karthikeyan Chinnakonda <kartthikspiky@gmail.com>
2018-07-25 13:04:38 +0530

    Modified the send-email function for multiple-recipients in "to"

--- src/sendgrid/core.clj
+++ src/sendgrid/core.clj
@@ -11,11 +11,17 @@
 
 (defn send-email
   [{:keys [from to subject message api-token filename content]}]
-  (let [json-body {:personalizations [{:to [{"email" to}]}]
+  (let [mail-recipients (if (coll? to)
+                          [{:to (mapv (fn [add]
+                                         {"email" add})
+                                       to)}]
+                          [{:to [{"email" to}]}])
+        json-body {:personalizations mail-recipients
                    :from    {"email" from}
                    :subject  subject
                    :content [{:type "text/html" :value  message}]}
-        attachments (when (and filename content) {:attachments [{:filename filename :content content}]})
+        attachments (when (and filename content)
+                      {:attachments [{:filename filename :content content}]})
         request {:content-type :json
                  :headers {:authorization api-token}
                  :body (write-str (merge json-body attachments))}]
